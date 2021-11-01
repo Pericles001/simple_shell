3abdc42522cabe5b891ae6d00544232d2d4c044d
pericles001 <periclesadjovi@outlook.fr>
2021-11-01 23:32:05 +0100

    Done with buitins

---
+++ builtin_functions.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+
+/**
+ *env - prints the current_environnement
+ *@tokenized_command: command entered
+ *
+ *Return: void
+ */
+
+void env(char **tokenized_command __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0, environ[i] != NULL; i++)
+	{
+		print(environ[i], STDOUT_FILENO);
+		print("\n", STDOUT_FILENO);
+	}
+}
+
+/**
+ * Quit - exits the shell
+ * @tokenized command entered
+ *
+ * Return: void
+ */
+
+void quit(char **tokenized_command)
+{
+	int num_token = 0, arg; 
+
+	for (; tokenized_command[num_token] != NULL; num_token++)
+		;
+	if (num_token == 1)
+	{
+		free(tokenize_command);
+		free(line);
+		free(commands);
+		exit(status);
+	}
+	else if (num_token == 2)
+	{
+		arg = _atoi(tokenized_command[1]);
+		if (arg == -1)
+		{
+			print(shell_name, STDERR_FILENO);
+			print(": 1: exit: Illegal number: ", STDERR_FILENO);
+			print(tokenized_command[1], STDERR_FILENO);
+			print("\n", STDERR_FILENO);
+			status = 2;
+		}
+		else
+		{
+			free(line);
+			free(tokenized_command);
+			free(commands);
+			exit(arg);
+		}
+	}
+	else
+		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
+}
