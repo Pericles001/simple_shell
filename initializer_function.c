21325dcea8f06fd7e5de86913b3bfb7703d1f109
pericles001 <periclesadjovi@outlook.fr>
2021-11-04 16:39:00 +0100

    Create initializer function

---
+++ initializer_function.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * initializer - starts executing everything
+ * @current_command: try to check current token
+ * @type_command: parse token
+ *
+ * Return: void function
+ */
+
+void initializer(char **current_command, int type_command)
+{
+	pid_t PID;
+
+	if (type_command == EXTERNAL_COMMAND || type_command == PATH_COMMAND)
+	{
+		PID = fork();
+		if (PID == 0)
+			execute_command(current_command, type_command);
+		else
+		{
+			waitpid(PID, &status, 0);
+			status >>= 8;
+		}
+	}
+	else
+		execute_command(current_command, type_command);
+}
